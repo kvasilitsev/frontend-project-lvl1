21f8d9a0fe2b7bc4ceb1352bc1b97aa423bf111e
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-04-07 20:23:51 -0300

    Step7 simplify gsd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,32 +3,18 @@ import { getRandomInt } from '../functions.js';
 import { gameLogic } from '../index.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
-
+const NOD = (x, y) => {
+	if (y > x) return NOD(y, x);
+	if (!y) return x;
+	return NOD(y, x % y);
+}
 const gameData = () => {
     const number1 = getRandomInt();
     const number2 = getRandomInt();
-    let correctAnswer;
+
+    let correctAnswer = String(NOD(number1, number2));
     let question = `${number1} ${number2}`;
-    if (number1 > number2){
-        for (let div = number2; div >= 1; div-=1){
-            if ((number1 % div === 0) && (number2 % div === 0)){
-                correctAnswer = String(div);
-                //console.log(div);
-                return cons(question, correctAnswer);
-            }
-        }
-    }
-    else if (number1 < number2){
-        for (let div = number1; div >= 1; div-=1){
-            if ((number1 % div === 0) && (number2 % div === 0)){
-                correctAnswer = String(div);
-                //console.log(div);
-                return cons(question, correctAnswer);
-               
-        }
-    }
-    }
-    correctAnswer = String(number1);
+        
     return cons(question, correctAnswer);
 }
 export default () => {
