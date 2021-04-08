f5db15a05e0eb8467f6ddf37c2cf05d17c4a8206
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-04-08 20:48:55 -0300

    Check fix greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import { printName } from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
-console.log('Hello' + ' ' + printName() + '!');
+console.log('Hello,' + ' ' + printName());
