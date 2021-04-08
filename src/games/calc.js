813c9e129842fabc6b1a660ffbee5b44351a3d48
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-04-08 20:53:01 -0300

    Check fix calc.js question syntax

--- src/games/calc.js
+++ src/games/calc.js
@@ -11,15 +11,15 @@ const gameData = () => {
   const getOpeartor = Math.random();
     if (getOpeartor < 0.34){
         correctAnswer = String(operand1 + operand2);
-        question = `${operand1}+${operand2}`;
+        question = `${operand1} + ${operand2}`;
     }
     else if (getOpeartor < 0.66 && getOpeartor >= 0.34){
         correctAnswer = String(operand1 - operand2);
-        question = `${operand1}-${operand2}`;
+        question = `${operand1} - ${operand2}`;
     }
     else {
         correctAnswer = String(operand1 * operand2);
-        question = `${operand1}*${operand2}`;
+        question = `${operand1} * ${operand2}`;
     }
     return cons(question, correctAnswer);
 }
