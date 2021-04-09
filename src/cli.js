0c37ca9cb0e10590288beaca3f4adfa1d6c4723e
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-04-08 21:16:34 -0300

    Check fix cli.js and functions.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
-const printName = () => {
-const userName = readlineSync.question('May I have your name?');
-return userName;
+
+export default () => {
+  const userName = readlineSync.question('May I have your name?');
+  return userName;
 };
-export { printName };
--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,2 @@
-export const getRandomInt = () => Math.floor(Math.random()*1000);
-export const getRandomIntDec = () => Math.floor(Math.random()*10);
-
-
-  
+export const getRandomInt = () => Math.floor(Math.random() * 1000);
+export const getRandomIntDec = () => Math.floor(Math.random() * 10);
