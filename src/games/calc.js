6653546260845a97b4145030235ea96827de596f
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-04-08 21:25:40 -0300

    Check fic calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,27 +4,26 @@ import { gameLogic } from '../index.js';
 const rules = 'What is the result of the expression?';
 
 const gameData = () => {
-  let correctAnswer; 
-  let question; 
+  let correctAnswer;
+  let question;
   const operand1 = getRandomInt();
   const operand2 = getRandomInt();
   const getOpeartor = Math.random();
-    if (getOpeartor < 0.34){
-        correctAnswer = String(operand1 + operand2);
-        question = `${operand1} + ${operand2}`;
-    }
-    else if (getOpeartor < 0.66 && getOpeartor >= 0.34){
-        correctAnswer = String(operand1 - operand2);
-        question = `${operand1} - ${operand2}`;
-    }
-    else {
-        correctAnswer = String(operand1 * operand2);
-        question = `${operand1} * ${operand2}`;
-    }
-    return cons(question, correctAnswer);
-}
+  if (getOpeartor < 0.34) {
+    correctAnswer = String(operand1 + operand2);
+    question = `${operand1} + ${operand2}`;
+  }
+  else if (getOpeartor < 0.66 && getOpeartor >= 0.34){
+    correctAnswer = String(operand1 - operand2);
+    question = `${operand1} - ${operand2}`;
+  }
+  else {
+    correctAnswer = String(operand1 * operand2);
+    question = `${operand1} * ${operand2}`;
+  }
+  return cons(question, correctAnswer);
+};
 
 export default () => {
-    gameLogic(rules, gameData);
-  };
-
+  gameLogic(rules, gameData);
+};
