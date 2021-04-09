3ac58b93a9dc48696766825d30d0adc5c9af193d
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-04-08 21:36:33 -0300

    Check fix calc.js -2

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,6 +1,7 @@
 import { cons } from '@hexlet/pairs';
 import { getRandomInt } from '../functions.js';
 import { gameLogic } from '../index.js';
+
 const rules = 'What is the result of the expression?';
 
 const gameData = () => {
@@ -11,12 +12,10 @@ const gameData = () => {
   const getOpeartor = Math.random();
   if (getOpeartor < 0.34) {
     correctAnswer = String(operand1 + operand2);
-    question = `${operand1} + ${operand2}`;
-  }
-  else if (getOpeartor < 0.66 && getOpeartor >= 0.34){
+    question = `${operand1} + ${operand2}`; }
+  else if (getOpeartor < 0.66 && getOpeartor >= 0.34) {
     correctAnswer = String(operand1 - operand2);
-    question = `${operand1} - ${operand2}`;
-  }
+    question = `${operand1} - ${operand2}`; }
   else {
     correctAnswer = String(operand1 * operand2);
     question = `${operand1} * ${operand2}`;
