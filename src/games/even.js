397deae51d3229126ee1cbf9210fb9e2fd1a9cd9
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-04-09 21:23:10 -0300

    check fix even.js and gcd.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,7 @@
+import { cons } from '@hexlet/pairs';
 import { getRandomInt } from '../functions.js';
 import { gameLogic } from '../index.js';
-import { cons } from '@hexlet/pairs';
+
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
@@ -8,14 +9,9 @@ const isEven = (number) => number % 2 === 0;
 const gameData = () => {
   const question = getRandomInt();
   const correctAnswer = isEven(question) ? 'yes' : 'no';
-    return cons(question, correctAnswer);
-}
+  return cons(question, correctAnswer);
+};
 
 export default () => {
-    gameLogic(rules, gameData);
-  };
-
-
-
-
-
+  gameLogic(rules, gameData);
+};
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,19 +4,18 @@ import { gameLogic } from '../index.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 const NOD = (x, y) => {
-	if (y > x) return NOD(y, x);
-	if (!y) return x;
-	return NOD(y, x % y);
-}
+  if (y > x) return NOD(y, x);
+  if (!y) return x;
+  return NOD(y, x % y);
+};
 const gameData = () => {
-    const number1 = getRandomInt();
-    const number2 = getRandomInt();
+  const number1 = getRandomInt();
+  const number2 = getRandomInt();
 
-    let correctAnswer = String(NOD(number1, number2));
-    let question = `${number1} ${number2}`;
-        
-    return cons(question, correctAnswer);
-}
+  let correctAnswer = String(NOD(number1, number2));
+  const question = `${number1} ${number2}`;        
+  return cons(question, correctAnswer);
+};
 export default () => {
-    gameLogic(rules, gameData);
-  };
+  gameLogic(rules, gameData);
+};
